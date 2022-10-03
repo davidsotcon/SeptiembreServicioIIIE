995f35f607251cf1164f3358901e1106f89d5eb8
david sotelo <david.sotcon@gmail.com>
2022-10-02 20:33:09 -0500

    prueba1

---
+++ src/main/java/com/example/demo/SeptiembreIiie2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeptiembreIiie2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeptiembreIiie2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/repository/ProductoRepositoryImp.java
@@ -0,0 +1,56 @@
+package com.example.demo.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Producto;
+
+@Service
+public class ProductoRepositoryImp implements ProductoRepository {
+	
+	List<Producto> almacenamiento = new ArrayList<>();
+
+	@Override
+	public void guardar(Producto producto) {
+		// TODO Auto-generated method stub
+		almacenamiento.add(producto);
+
+	}
+
+	@Override
+	public void actualizar(Producto producto) {
+		// TODO Auto-generated method stub
+		Producto existeactualizar = obtener(producto.getIdProducto());
+		eliminar(producto.getIdProducto());
+		almacenamiento.add(existeactualizar);
+
+	}
+
+	@Override
+	public void eliminar(Integer id) {
+		// TODO Auto-generated method stub
+		Producto existeEliminar = obtener(id);
+		almacenamiento.remove(existeEliminar);
+	}
+
+	@Override
+	public List<Producto> listar() {
+		// TODO Auto-generated method stub
+		return almacenamiento;
+	}
+
+	@Override
+	public Producto obtener(Integer id) {
+		// TODO Auto-generated method stub
+		//for (Producto producto : almacenamiento) {
+		//	if(producto.getIdProducto()==id) {
+		//		return producto;
+		//	}
+		//}
+		//return null;
+		return almacenamiento.stream().filter(p->p.getIdProducto()==id).findFirst().orElse(null);
+	}
+
+}
